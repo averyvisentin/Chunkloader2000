9afdd41edd22a9db2b2cd7ce93502660499bfd0f
Avery Visentin <averyvisentin@gmail.com>
2024-07-14 01:50:43 -0400

    file paths

--- turtle/install.lua
+++ turtle/install.lua
@@ -1,12 +1,22 @@
+local diskPath = 'disk/turtle/'
+local apiPath = '/apis/'
 
-if fs.exists('/apis') then
-    fs.delete('/apis')
+-- Check existence and copy of basics.lua
+if fs.exists(diskPath .. 'basics.lua') then
+    fs.copy(diskPath .. 'basics.lua', apiPath .. 'basics.lua')
+    print('Copied basics.lua')
+else
+    print('Error: basics.lua not found in ' .. diskPath)
 end
-fs.makeDir('/apis')
-fs.copy('state.lua', '/apis/state')
-fs.copy('basics.lua', '/apis/basics')
-fs.copy('actions.lua', '/apis/actions')
-fs.copy('config.lua', '/apis/config')
-fs.copy('rednet.lua', '/apis/inout')
-fs.copy('startup.lua', '/apis/startup')
-fs.copy('chico.lua', '/apis/chico')
+
+if fs.exists('/apis/') then
+    fs.delete('/apis/')
+end
+fs.makeDir('/apis/')
+fs.copy('/state.lua', '/apis/state')
+fs.copy('/basics.lua', '/apis/basics')
+fs.copy('/actions.lua', '/apis/actions')
+fs.copy('/config.lua', '/apis/config')
+fs.copy('/rednet.lua', '/apis/inout')
+fs.copy('/startup.lua', '/apis/startup')
+fs.copy('/chico.lua', '/apis/chico')
