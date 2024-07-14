96ca35fba65d8b52790203fb0b89c46cc6425f28
Avery Visentin <averyvisentin@gmail.com>
2024-07-14 01:53:43 -0400

    hmmmmmm file paths

--- turtle/install.lua
+++ turtle/install.lua
@@ -1,22 +1,36 @@
 local diskPath = 'disk/turtle/'
 local apiPath = '/apis/'
 
--- Check existence and copy of basics.lua
-if fs.exists(diskPath .. 'basics.lua') then
-    fs.copy(diskPath .. 'basics.lua', apiPath .. 'basics.lua')
-    print('Copied basics.lua')
-else
-    print('Error: basics.lua not found in ' .. diskPath)
+-- Ensure /apis/ exists and copy necessary files
+if fs.exists(apiPath) then
+    fs.delete(apiPath)
+end
+fs.makeDir(apiPath)
+
+-- Copy API files
+local function copyFile(source, destination)
+    if fs.exists(source) then
+        fs.copy(source, destination)
+        print('Copied ' .. source .. ' to ' .. destination)
+    else
+        print('Error: ' .. source .. ' not found.')
+    end
 end
 
-if fs.exists('/apis/') then
-    fs.delete('/apis/')
-end
-fs.makeDir('/apis/')
-fs.copy('/state.lua', '/apis/state')
-fs.copy('/basics.lua', '/apis/basics')
-fs.copy('/actions.lua', '/apis/actions')
-fs.copy('/config.lua', '/apis/config')
-fs.copy('/rednet.lua', '/apis/inout')
-fs.copy('/startup.lua', '/apis/startup')
-fs.copy('/chico.lua', '/apis/chico')
+copyFile(diskPath .. 'state.lua', apiPath .. 'state.lua')
+copyFile(diskPath .. 'basics.lua', apiPath .. 'basics.lua')
+copyFile(diskPath .. 'actions.lua', apiPath .. 'actions.lua')
+copyFile(diskPath .. 'config.lua', apiPath .. 'config.lua')
+copyFile(diskPath .. 'rednet.lua', apiPath .. 'inout.lua')
+copyFile(diskPath .. 'startup.lua', apiPath .. 'startup.lua')
+copyFile(diskPath .. 'chico.lua', apiPath .. 'chico.lua')
+
+-- Load APIs
+os.loadAPI(apiPath .. 'state.lua')
+os.loadAPI(apiPath .. 'basics.lua')
+os.loadAPI(apiPath .. 'actions.lua')
+os.loadAPI(apiPath .. 'config.lua')
+os.loadAPI(apiPath .. 'inout.lua')
+
+-- Additional debug output
+print('APIs loaded successfully.')
